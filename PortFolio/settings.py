d0267b2626ec7b1237ad22d227f6faed178e099d
Suriya Prakash <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-12-24 10:19:53 +0530

    add database

--- PortFolio/settings.py
+++ PortFolio/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from os import getenv
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,13 +84,21 @@ WSGI_APPLICATION = 'PortFolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
+# Replace the DATABASES section of your settings.py with this
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+path = tmpPostgres.path.decode('utf-8') if isinstance(tmpPostgres.path, bytes) else tmpPostgres.path
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': path.lstrip('/'),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
